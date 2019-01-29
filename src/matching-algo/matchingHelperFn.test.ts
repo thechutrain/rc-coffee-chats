ba964a4cfc4e302472f94cd1ca32723a07a5b8ca
thechutrain <chu.chuyalan@gmail.com>
2019-01-28 20:48:11 -0500

    way cleaner and more simple, could abstract the filtering for later

--- src/matching-algo/matchingHelperFn.test.ts
+++ src/matching-algo/matchingHelperFn.test.ts
@@ -1,124 +1,247 @@
-import { findUniqueMatches, getLeastRecentMatches } from './matchingHelperFn';
+import {
+  filterForUniqueMatches,
+  filterForPrevMatches
+} from './matchingHelperFn';
 import { IUser, IpastMatchObj, prevMatch } from './matching-algo';
 
-// previous findUniqueMatchers
-describe('matching algorithm helper function: findUniqueMatchers', () => {
-  it('should not find any new matched pairs from empty pool of unmatched users ', () => {
-    const currUser: IUser = Object.freeze({
-      email: 'test@rc.com',
-      full_name: 'test email',
-      prevMatches: []
-    });
-    const poolOfUnmatchedUsers = [];
+// const currUser: IUser = Object.freeze({
+//   email: 'test@rc.com',
+//   full_name: 'test email',
+//   prevMatches: []
+// });
+describe('match-algo-helper-fn: filterForUniqueMatches', () => {
+  it('should return not return any users if no available users', () => {
+    const prevMatches: prevMatch[] = [];
+    const poolOfAvailableUsers: IUser[] = [];
 
-    const { prevMatches } = currUser;
-
-    const newMatchedPairs = findUniqueMatches(
-      prevMatches,
-      poolOfUnmatchedUsers
-    );
-
-    expect(newMatchedPairs).toEqual([]);
+    const received = filterForUniqueMatches(prevMatches, poolOfAvailableUsers);
+    expect(received).toEqual([]);
   });
-
-  it('should return everyone in the pool of unmatched users, if user has no previous matches', () => {
-    const currUser: IUser = Object.freeze({
-      email: 'test@rc.com',
-      full_name: 'test email',
-      prevMatches: []
-    });
-    const poolOfUnMatchedUsers: IUser[] = [
-      {
-        email: 'a@rc.com',
-        full_name: 'a bot',
-        prevMatches: []
-      },
-      {
-        email: 'b@rc.com',
-        full_name: 'b bot',
-        prevMatches: []
-      }
-    ];
-
-    const { prevMatches } = currUser;
-
-    const newMatchedPairs = findUniqueMatches(
-      prevMatches,
-      poolOfUnMatchedUsers
-    );
-
-    expect(newMatchedPairs).toEqual(poolOfUnMatchedUsers);
-  });
-
-  it('should find only the the user that they were not matched with', () => {
-    const userA = {
+  it('should not return any user, if only available user was a prev match', () => {
+    const dateOfMatch = new Date(1);
+    const userA: IUser = {
       email: 'a@rc.com',
-      full_name: 'a bot',
-      prevMatches: []
-    };
-    const userB = {
-      email: 'b@rc.com',
-      full_name: 'b bot',
-      prevMatches: []
-    };
-    const currUser: IUser = Object.freeze({
-      email: 'test@rc.com',
-      full_name: 'test email',
+      full_name: 'a test',
       prevMatches: [
         {
-          email: userA.email,
-          matchDate: new Date()
+          matchDate: dateOfMatch,
+          email: 'b@rc.com'
         }
       ]
-    });
-    const poolOfUnMatchedUsers: IUser[] = [userA, userB];
+    };
+    const userB: IUser = {
+      email: 'b@rc.com',
+      full_name: 'b test',
+      prevMatches: [
+        {
+          matchDate: dateOfMatch,
+          email: 'a@rc.com'
+        }
+      ]
+    };
+    const prevMatches: prevMatch[] = userB.prevMatches; // Looking for B's unique matches
+    const poolOfAvailableUsers: IUser[] = [userA];
 
-    const { prevMatches } = currUser;
+    const received = filterForUniqueMatches(prevMatches, poolOfAvailableUsers);
+    expect(received).toEqual([]);
+  });
 
-    const newMatchedPairs = findUniqueMatches(
-      prevMatches,
-      poolOfUnMatchedUsers
-    );
+  it('should return the only available user if they were not a previous match', () => {
+    const dateOfMatch = new Date(1);
+    const userA: IUser = {
+      email: 'a@rc.com',
+      full_name: 'a test',
+      prevMatches: []
+    };
+    const userB: IUser = {
+      email: 'b@rc.com',
+      full_name: 'b test',
+      prevMatches: []
+    };
+    const prevMatches: prevMatch[] = userB.prevMatches; // prevMatches of userB
+    const poolOfAvailableUsers: IUser[] = [userA];
 
-    expect(newMatchedPairs).toEqual([userB]);
+    const received = filterForUniqueMatches(prevMatches, poolOfAvailableUsers);
+    expect(received).toEqual([userA]);
   });
 });
+
+describe('match-algo-helper-fn: filterForPrevMatches', () => {
+  it('should not return any users if no available users', () => {
+    const prevMatches: prevMatch[] = [];
+    const poolOfAvailableUsers: IUser[] = [];
+
+    const received = filterForPrevMatches(prevMatches, poolOfAvailableUsers);
+    expect(received).toEqual([]);
+  });
+
+  it('should not return any users, if the only availabe user was not a prev match', () => {
+    const dateOfMatch = new Date(1);
+    const userA: IUser = {
+      email: 'a@rc.com',
+      full_name: 'a test',
+      prevMatches: []
+    };
+    const userB: IUser = {
+      email: 'b@rc.com',
+      full_name: 'b test',
+      prevMatches: []
+    };
+    const prevMatches: prevMatch[] = userB.prevMatches; // Looking for B's unique matches
+    const poolOfAvailableUsers: IUser[] = [userA];
+
+    const received = filterForPrevMatches(prevMatches, poolOfAvailableUsers);
+    expect(received).toEqual([]);
+  });
+  it('should return the available user if that user was a prev match', () => {
+    const dateOfMatch = new Date(1);
+    const userA: IUser = {
+      email: 'a@rc.com',
+      full_name: 'a test',
+      prevMatches: [
+        {
+          matchDate: dateOfMatch,
+          email: 'b@rc.com'
+        }
+      ]
+    };
+    const userB: IUser = {
+      email: 'b@rc.com',
+      full_name: 'b test',
+      prevMatches: [
+        {
+          matchDate: dateOfMatch,
+          email: 'a@rc.com'
+        }
+      ]
+    };
+    const prevMatches: prevMatch[] = userB.prevMatches; // Looking for B's unique matches
+    const poolOfAvailableUsers: IUser[] = [userA];
+
+    const received = filterForPrevMatches(prevMatches, poolOfAvailableUsers);
+    expect(received).toEqual([userA]);
+  });
+  // it('should not be able to return any one outside of the available users if that user was a prev match', () => {});
+});
+
+// previous findUniqueMatchers
+// describe('matching algorithm helper function: findUniqueMatchers', () => {
+//   it('should not find any new matched pairs from empty pool of unmatched users ', () => {
+//     const currUser: IUser = Object.freeze({
+//       email: 'test@rc.com',
+//       full_name: 'test email',
+//       prevMatches: []
+//     });
+//     const poolOfAvailableUsers = [];
+
+//     const { prevMatches } = currUser;
+
+//     const newMatchedPairs = findUniqueMatches(
+//       prevMatches,
+//       poolOfAvailableUsers
+//     );
+
+//     expect(newMatchedPairs).toEqual([]);
+//   });
+
+//   it('should return everyone in the pool of unmatched users, if user has no previous matches', () => {
+//     const currUser: IUser = Object.freeze({
+//       email: 'test@rc.com',
+//       full_name: 'test email',
+//       prevMatches: []
+//     });
+//     const poolOfAvailableUsers: IUser[] = [
+//       {
+//         email: 'a@rc.com',
+//         full_name: 'a bot',
+//         prevMatches: []
+//       },
+//       {
+//         email: 'b@rc.com',
+//         full_name: 'b bot',
+//         prevMatches: []
+//       }
+//     ];
+
+//     const { prevMatches } = currUser;
+
+//     const newMatchedPairs = findUniqueMatches(
+//       prevMatches,
+//       poolOfAvailableUsers
+//     );
+
+//     expect(newMatchedPairs).toEqual(poolOfAvailableUsers);
+//   });
+
+//   it('should find only the the user that they were not matched with', () => {
+//     const userA = {
+//       email: 'a@rc.com',
+//       full_name: 'a bot',
+//       prevMatches: []
+//     };
+//     const userB = {
+//       email: 'b@rc.com',
+//       full_name: 'b bot',
+//       prevMatches: []
+//     };
+//     const currUser: IUser = Object.freeze({
+//       email: 'test@rc.com',
+//       full_name: 'test email',
+//       prevMatches: [
+//         {
+//           email: userA.email,
+//           matchDate: new Date()
+//         }
+//       ]
+//     });
+//     const poolOfAvailableUsers: IUser[] = [userA, userB];
+
+//     const { prevMatches } = currUser;
+
+//     const newMatchedPairs = findUniqueMatches(
+//       prevMatches,
+//       poolOfAvailableUsers
+//     );
+
+//     expect(newMatchedPairs).toEqual([userB]);
+//   });
+// });
 
 // Test for getLeastRecentMatch
-describe('matching algorithm helper fn: getLeastRecentMatch', () => {
-  it('should return an empty array if no one in poolOfMatches', () => {
-    const currUser: IUser = Object.freeze({
-      email: 'test@rc.com',
-      full_name: 'test email',
-      prevMatches: []
-    });
+// describe('matching algorithm helper fn: getLeastRecentMatch', () => {
+//   it('should return an empty array if no one in poolOfMatches', () => {
+//     const currUser: IUser = Object.freeze({
+//       email: 'test@rc.com',
+//       full_name: 'test email',
+//       prevMatches: []
+//     });
 
-    const { prevMatches } = currUser;
-    const poolOfMatches = [];
+//     const { prevMatches } = currUser;
+//     const poolOfMatches = [];
 
-    const leastRecentMatches = getLeastRecentMatches(
-      prevMatches,
-      poolOfMatches
-    );
+//     const leastRecentMatches = getLeastRecentMatches(
+//       prevMatches,
+//       poolOfMatches
+//     );
 
-    expect(leastRecentMatches).toEqual([]);
-  });
+//     expect(leastRecentMatches).toEqual([]);
+//   });
 
-  it('should return everyone in the poolOfMatches', () => {
-    const currUser: IUser = Object.freeze({
-      email: 'test@rc.com',
-      full_name: 'test email',
-      prevMatches: []
-    });
+//   it('should return everyone in the poolOfMatches', () => {
+//     const currUser: IUser = Object.freeze({
+//       email: 'test@rc.com',
+//       full_name: 'test email',
+//       prevMatches: []
+//     });
 
-    const { prevMatches } = currUser;
-    const poolOfMatches = [];
+//     const { prevMatches } = currUser;
+//     const poolOfMatches = [];
 
-    const leastRecentMatches = getLeastRecentMatches(
-      prevMatches,
-      poolOfMatches
-    );
+//     const leastRecentMatches = getLeastRecentMatches(
+//       prevMatches,
+//       poolOfMatches
+//     );
 
-    expect(leastRecentMatches).toEqual([]);
-  });
-});
+//     expect(leastRecentMatches).toEqual([]);
+//   });
+// });
--- src/matching-algo/matchingHelperFn.ts
+++ src/matching-algo/matchingHelperFn.ts
@@ -1,26 +1,50 @@
 import { email, IUser, IpastMatchObj, prevMatch } from './matching-algo';
 
-export function filterForUniqueMatches(): IUser[] {
-  return [];
+export function filterForUniqueMatches(
+  prevMatches: prevMatch[],
+  poolOfAvailableUsers: IUser[]
+): IUser[] {
+  return poolOfAvailableUsers.filter((availableUser: IUser) => {
+    const { email: availableEmail } = availableUser;
+
+    const wasPrevMatch = prevMatches.some(({ email: prevMatchEmail }) => {
+      return prevMatchEmail === availableEmail;
+    });
+
+    return !wasPrevMatch;
+  });
 }
-export function filterForPrevMatches(): IUser[] {
-  return [];
+export function filterForPrevMatches(
+  prevMatches: prevMatch[],
+  poolOfAvailableUsers: IUser[]
+): IUser[] {
+  return poolOfAvailableUsers.filter((availableUser: IUser) => {
+    const { email: availableEmail } = availableUser;
+
+    const wasPrevMatch = prevMatches.some(({ email: prevMatchEmail }) => {
+      return prevMatchEmail === availableEmail;
+    });
+
+    return wasPrevMatch;
+  });
 }
 
 export function sortByOldestMatch(prevMatches: prevMatch[]): prevMatch[] {
   return [];
 }
+
+// ==========================================
 // TODO: Make this more generic, pass in a flag to determine whether I want unique or prev matched users
 // filterForUniqueMatches
 export function findUniqueMatches(
   prevMatches: prevMatch[],
-  poolOfUnmatchedUsers: IUser[]
+  poolOfAvailableUsers: IUser[]
 ): IUser[] {
   if (prevMatches.length === 0) {
-    return poolOfUnmatchedUsers;
+    return poolOfAvailableUsers;
   }
 
-  return poolOfUnmatchedUsers.filter(currUnmatchedUser => {
+  return poolOfAvailableUsers.filter(currUnmatchedUser => {
     // if any of the prevMatches are the same as currUnmatchedUser,
     // then return false (filter it out of the pool)
     return !prevMatches.some((currPrevMatch: prevMatch) => {
@@ -32,14 +56,14 @@ export function findUniqueMatches(
 
 export function filterUserPool(
   prevMatches: prevMatch[],
-  poolOfUnmatchedUsers: IUser[],
+  poolOfAvailableUsers: IUser[],
   filterBy
 ): IUser[] {
   if (prevMatches.length === 0) {
-    return poolOfUnmatchedUsers;
+    return poolOfAvailableUsers;
   }
 
-  return poolOfUnmatchedUsers.filter(currUnmatchedUser => {
+  return poolOfAvailableUsers.filter(currUnmatchedUser => {
     // if any of the prevMatches are the same as currUnmatchedUser,
     // then return false (filter it out of the pool)
     return !prevMatches.some((currPrevMatch: prevMatch) => {
