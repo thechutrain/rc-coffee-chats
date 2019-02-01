06476e08dd87647c9a809d988b6a1cca0814c93a
thechutrain <chu.chuyalan@gmail.com>
2019-02-01 10:32:35 -0500

    added working definitions need to define

---
+++ src/zulip-interface/zulipCli.d.ts
@@ -0,0 +1,30 @@
+/**
+ * CLI commands:
+ * - UPDATE/CHANGE:
+ *  - ? <rm-day> - Mon Tues Wed Thu Fri Sat Sun
+ *  - ? <add-day> - Mon tues wed thu fri sat sun
+ *  - <days> - Mon tues wed thu fri sat sun
+ *  - <skip> - true | false
+ *  - <warnings> - yes | y | no | n | true | false
+ *  later....
+ *  - <active> - yes | y | true | 1 | no | n | false | 0
+ * - STATUS/INFO/SHOW:
+ *  - <help> --> prints out help
+ *  - <days>
+ *  - <next>
+ *  - <warnings>
+ *
+ * HELP -- cmd
+ * skip --undo
+ *
+ * SHOW days
+ * SHOW next
+ *
+ * Version 1:
+ * - help
+ * - next
+ * - days [options day]
+ * - skip [option true/false]
+ */
+
+export enum CliCommand {}
