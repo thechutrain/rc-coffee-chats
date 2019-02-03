c2797d6d1a7128b72a0cefdff17faa258edf71bb
thechutrain <chu.chuyalan@gmail.com>
2019-02-02 21:11:53 -0500

    renamed zulipMessenger file to sendMessage

--- src/zulip_coms/sendMessage.ts
+++ src/zulip_coms/sendMessage.ts
@@ -6,16 +6,19 @@
 import * as zulip from 'zulip-js';
 import { IZulipConfig } from './interface';
 
-export async function initZulipMessenger(
+// TODO: this doesn't need to be async or a promise
+export function initZulipMessenger(
   zulipConfig: IZulipConfig = {}
-): Promise<any> {
+): {
+  sendMessage: (toEmail: string | string[], messageContent) => Promise<any>;
+} {
   const config = {
     username: zulipConfig.ZULIP_USERNAME || process.env.ZULIP_USERNAME,
     apiKey: zulipConfig.ZULIP_API_KEY || process.env.ZULIP_API_KEY,
     realm: zulipConfig.ZULIP_REALM || process.env.ZULIP_REALM
   };
 
-  const zulipAPI = await zulip(config);
+  const zulipAPI = zulip(config);
 
   // Send a message to a given Zulip user
   function sendMessage(toEmail: string, messageContent: string);
