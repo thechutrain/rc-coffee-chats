9999d2ec3c0ec211969b50b2625877b7eb53497d
thechutrain <chu.chuyalan@gmail.com>
2019-02-04 23:36:41 -0500

    match table just id & date. TODO: make pk composite of user 1&2 & date. Simply using id not match_id

--- src/db/match.ts
+++ src/db/match.ts
@@ -24,15 +24,11 @@ export function initMatchModel(db: sqlite): any {
   // createTable();
 
   function createTable(): void {
+    // TODO: add raincheck column
+    // NOTE: primary key, composite of user_1, user_2, & date
     const query = `CREATE TABLE IF NOT EXISTS Match (
-      match_id INTEGER PRIMARY KEY NOT NULL UNIQUE,
-      user_1_id INTEGER NOT NULL,
-      user_2_id INTEGER NOT NULL,
-      date TEXT NOT NULL,
-      FOREIGN KEY (user_1_id) REFERENCES User (user_id)
-      ON DELETE CASCADE ON UPDATE NO ACTION,
-    FOREIGN KEY (user_2_id) REFERENCES User (user_id)
-      ON DELETE CASCADE ON UPDATE NO ACTION
+      id INTEGER PRIMARY KEY NOT NULL UNIQUE,
+      date TEXT NOT NULL
     )`;
 
     // const query = `CREATE TABLE IF NOT EXISTS Match (
@@ -52,8 +48,8 @@ export function initMatchModel(db: sqlite): any {
   }
 
   function count(): number {
-    const stmt = db.prepare(`SELECT COUNT(match_id) FROM Match`);
-    const { 'COUNT(match_id)': numRecord } = stmt.get();
+    const stmt = db.prepare(`SELECT COUNT(id) FROM Match`);
+    const { 'COUNT(id)': numRecord } = stmt.get();
     return numRecord;
   }
 
@@ -62,13 +58,13 @@ export function initMatchModel(db: sqlite): any {
   // - fn: getMatches by day?
   // - fn: get all matches for particular user?
   // - fn: get match by match_id
-  function findAllUserMatches(targetUser: number): IMatchDB {
-    const findStmt = db.prepare(
-      `SELECT * FROM Match WHERE user_1_id = ? OR user_2_id = ?`
-    );
-    // let matches = [];
-    return findStmt.all(targetUser, targetUser);
-  }
+  // function findAllUserMatches(targetUser: number): IMatchDB {
+  //   const findStmt = db.prepare(
+  //     `SELECT * FROM Match WHERE user_1_id = ? OR user_2_id = ?`
+  //   );
+  //   // let matches = [];
+  //   return findStmt.all(targetUser, targetUser);
+  // }
 
   // export interface IAddMatchArgs {
   //   user_1_id: number;
@@ -86,11 +82,11 @@ export function initMatchModel(db: sqlite): any {
     const date = matchArgs.date || new Date().toISOString().split('T')[0];
 
     const insertMatchQuery = db.prepare(`
-    INSERT INTO Match (user_1_id, user_2_id, date) VALUES (?, ?, ?)`);
+    INSERT INTO Match (date) VALUES (?)`);
     const {
       changes: rowsChanged,
       lastInsertROWID: newMatchId
-    } = insertMatchQuery.run(user_1_id, user_2_id, date);
+    } = insertMatchQuery.run(date);
 
     if (!rowsChanged) {
       throw new Error(`Could not insert into Match table`);
@@ -130,7 +126,7 @@ export function initMatchModel(db: sqlite): any {
     createTable,
     _deleteRecords,
     count,
-    find: findAllUserMatches,
+    // find: findAllUserMatches,
     add: addMatch
     // update: updateMatch // TODO: later
   };
