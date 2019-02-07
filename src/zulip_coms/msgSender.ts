33eabda7202c5ea0772a081c9b2e04b54d39824b
thechutrain <chu.chuyalan@gmail.com>
2019-02-07 09:48:30 -0500

    creating a message sender factory

--- src/zulip_coms/msgSender.ts
+++ src/zulip_coms/msgSender.ts
@@ -4,11 +4,43 @@
  */
 
 import axios from 'axios';
+import { ICliAction } from './interface';
 
 // TODO: make a sendMessage (takes in message type etc.)
+export enum messageType {
+  'SIGNUP',
+  'UPDATE_DAYS'
+}
+interface IMsgSenderArgs {
+  toEmail: string | string[];
+  status: 'OK' | 'ERROR';
+  messageType: messageType;
+  payload?: any;
+  message?: string;
+  cliAction?: ICliAction;
+}
 
-// TODO: change sendMessage --> sendGenericMessage
-export function sendMessage(
+export function zulipMsgSender(msgOpt: IMsgSenderArgs): void {
+  let messageContent;
+  switch (msgOpt.messageType) {
+    case messageType.SIGNUP:
+      if (msgOpt.status === 'OK') {
+        messageContent = `You've successfully been added to coffee chat!`;
+      } else {
+        messageContent = `Failed to sign you up`;
+      }
+
+    default:
+      messageContent = `Status: ${msgOpt.status}
+      payload: ${msgOpt.payload}
+      message: ${msgOpt.message}`;
+      break;
+  }
+
+  sendGenericMessage(msgOpt.toEmail, messageContent);
+}
+
+export function sendGenericMessage(
   toEmail: string | string[],
   messageContent: string
 ) {
@@ -36,13 +68,4 @@ export function sendMessage(
     },
     data: dataAsQueryParams
   });
-}
-
-export function sendErrorMessage(
-  toEmail: string | string[],
-  errorMessage: string
-) {
-  const messageContent = `ERROR: ${errorMessage}`;
-
-  return sendMessage(toEmail, messageContent);
 }
