26f55c65bd7a37ccb11fbdc555d36ecbac31cea8
thechutrain <chu.chuyalan@gmail.com>
2019-02-12 17:23:34 -0500

    random queries I was messing with

--- data/migrations/migration_tests.sql
+++ data/migrations/migration_tests.sql
@@ -3,7 +3,7 @@
 -- SELECT COUNT(email) from User where warning_exception = 1;
 
 -- Check number of users who are active:
-SELECT COUNT(email) from User where is_active = 1;
+-- SELECT COUNT(email) from User where is_active = 1;
 
 -- Checking number of User_Match entries:
 -- SELECT COUNT(id) from User_Match;
@@ -34,9 +34,9 @@ SELECT COUNT(email) from User where is_active = 1;
 
 --   select user_id from User_Match where match_id in alans_matches;
 
-with alans_matches as (select distinct(match_id) as mid from User_Match where user_id = (SELECT id from User where email="alancodes@gmail.com"))
+-- with alans_matches as (select distinct(match_id) as mid from User_Match where user_id = (SELECT id from User where email="alancodes@gmail.com"))
 
-  select user_id from User_Match where match_id in alans_matches;
+-- select count(user_id) from User_Match where match_id in alans_matches and user_id <> (SELECT id from User where email="alancodes@gmail.com");
 
 -- select count(distinct(user_id)) from User_Match where match_id in (select distinct(match_id) from User_Match where user_id = (SELECT id from User where email="recurse@dbalan.in"));
 
@@ -47,3 +47,40 @@ with alans_matches as (select distinct(match_id) as mid from User_Match where us
 --       where user_id = (SELECT id from User where email="recurse@dbalan.in")
 --      )
 --     );
+
+
+-- =============== WORKS ===================
+-- GETS THE NUMBER OF MATCHES YOU've HAD:
+
+-- with alans_matches as (select distinct(match_id) as mid from User_Match where user_id = (SELECT id from User where email="alancodes@gmail.com"))
+
+-- select count(user_id) from User_Match where match_id in alans_matches and user_id <> (SELECT id from User where email="alancodes@gmail.com");
+
+-- =============== WORKS ===================
+-- GETS THE USER NAMES OF YOUR MATCHES
+select U.email from User U WHERE U.id in (
+  with alans_matches as (select distinct(match_id) as mid from User_Match UM where UM.user_id = (SELECT id from User where email="alancodes@gmail.com"))
+
+  select UM.user_id from User_Match UM where UM.match_id in alans_matches and U.id <> (SELECT id from User where email="alancodes@gmail.com")
+);
+
+-- SELECT U.id, U.email, U.full_name, Match.date
+--       FROM User U
+--       LEFT Join User_Match
+--         ON U.id = User_Match.user_id
+--       LEFT JOIN Match
+--         ON User_Match.match_id = Match.id
+--       WHERE User_Match.user_id <> ${targetUserId}
+--       AND User_Match.match_id in (
+--         SELECT Match.id
+--         FROM User
+--         LEFT JOIN User_Match
+--           ON User.id = User_Match.user_id
+--         LEFT JOIN Match
+--           ON User_Match.match_id = Match.id
+--         WHERE User.id = ${targetUserId}
+--        )
+
+-- =============== TODO ===================
+
+
