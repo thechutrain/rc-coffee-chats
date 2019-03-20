5d46c53783436e6f4f5d19fbecb9f886e1416d4d
thechutrain <chu.chuyalan@gmail.com>
2019-03-20 17:10:17 -0400

    action creater is way more simple, takes cli & produces a valid action

--- src/middleware/action-creater.ts
+++ src/middleware/action-creater.ts
@@ -6,17 +6,47 @@ import * as types from '../types';
 
 export function actionCreater(req: types.IZulipRequest, res, next) {
   console.log('========= START of actionCreater middleware ==========');
+  const { isRegistered, email } = req.local.user;
+  let actionType: types.Action | null = null;
   // Case: not registered user
-  // 1) prompt signup
-  // 2) signup user
+  if (!isRegistered) {
+    const wantsToSignUp = req.body.data.match(/signup/gi);
+    actionType = wantsToSignUp
+      ? types.Action.REGISTER
+      : types.Action.PROMPT_SIGNUP;
+  } else {
+    // Case: registered user
+    // 1) valid command
+    // 2) not valid command
+    actionType = getAction(req.local.cli);
+  }
 
-  // Case: registered user
-  // 1) valid command
-  // 2) not valid command
   req.local.action = {
-    type: null,
-    currentUser: req.local.user.email
+    type: actionType,
+    currentUser: email
   };
+
+  console.log(req.local.action);
   console.log('========= END of actionCreater middleware ==========');
   next();
+}
+
+// export function isValidCli(cli: types.IParsedCmd): boolean {
+//   const command = cli.subcommand
+//     ? `${cli.directive}_${cli.subcommand}`
+//     : `${cli.directive}`;
+
+//   return command in types.Action;
+// }
+
+export function getAction(cli: types.IParsedCmd): types.Action | null {
+  const command = cli.subcommand
+    ? `${cli.directive}_${cli.subcommand}`
+    : `${cli.directive}`;
+
+  if (command in types.Action) {
+    return types.Action[command];
+  } else {
+    return null;
+  }
 }
