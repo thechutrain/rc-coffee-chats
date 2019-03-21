05d5e4f9679d750dab29dd632e4acd5d20352e1e
thechutrain <chu.chuyalan@gmail.com>
2019-03-21 19:35:54 -0400

    sweeeet! creates table from fields & table name

--- src/db/model.ts
+++ src/db/model.ts
@@ -2,17 +2,57 @@ import sqlite from 'better-sqlite3';
 import * as types from './dbTypes';
 
 export abstract class Model {
-  protected abstract tableName: string; // ex. User
-  protected db;
-  protected abstract fields: types.fields;
+  static db: sqlite;
+  // NOTE: add tableName, fields should be part of an interface!
+  protected tableName: string; // ex. User
+  protected fields: types.fields;
 
-  constructor(db) {
-    this.db = db;
+  static createTable(tableName: string, fields: types.fields, relations?: any) {
+    if (!Model.db) {
+      throw new Error(`No database intialized`);
+    }
+    // 1) Get each field pertaining to column, ex. username TEXT NOT NULL,
+    const fieldsAsArray: string[] = Object.keys(fields).map(field => {
+      const { type, isPrimaryKey, isUnique, isNotNull, defaultValue } = fields[
+        field
+      ];
+      let fieldStr = `${field} ${type}`;
+      if (isPrimaryKey) {
+        fieldStr = fieldStr + ' PRIMARY KEY';
+      }
+      if (isUnique) {
+        fieldStr = fieldStr + ' UNIQUE';
+      }
+      if (isNotNull) {
+        fieldStr = fieldStr + ' NOT NULL';
+      }
+      if (defaultValue && defaultValue !== '') {
+        fieldStr = fieldStr + ' DEFAULT ' + defaultValue;
+      }
 
-    return this;
+      return fieldStr;
+    });
+
+    if (fieldsAsArray.length === 0) {
+      throw new Error('Must have at least one field in the table');
+    }
+
+    const queryBody = fieldsAsArray.join(',\n');
+    const query = `CREATE TABLE IF NOT EXISTS ${tableName} (${queryBody})`;
+
+    console.log(query);
+    Model.db.exec(query);
   }
+
+  constructor(db: sqlite) {
+    if (!Model.db) {
+      Model.db = db;
+    }
+  }
+
   public count(): number {
-    const countQuery = this.db.prepare(
+    // Note: this will point to actual child usermodel ect. :)
+    const countQuery = Model.db.prepare(
       `SELECT COUNT(id) FROM ${this.tableName}`
     );
 
@@ -20,6 +60,4 @@ export abstract class Model {
 
     return numRecord;
   }
-
-  public abstract createTable();
 }
--- src/db/user.ts
+++ src/db/user.ts
@@ -2,12 +2,9 @@ import sqlite from 'better-sqlite3';
 import { Model } from './model';
 
 import * as path from 'path';
-
-// import { castBoolInt } from '../utils/index';
-
-// export const SqliteType = ['NULL', 'INTEGER', 'REAL', 'TEXT', 'BLOB'];
-
 import * as types from './dbTypes';
+
+export const TABLE_NAME = 'User';
 export const UserModelFields: types.fields = {
   id: {
     type: 'INTEGER',
@@ -43,66 +40,66 @@ export const UserModelFields: types.fields = {
 };
 
 export class UserModel extends Model {
-  protected readonly tableName = 'User';
+  protected readonly tableName = TABLE_NAME;
   protected fields: types.fields = UserModelFields;
 
   constructor(db) {
     super(db);
 
-    this.createTable();
+    Model.createTable(this.tableName, this.fields);
   }
 
-  public createTable() {
-    // const query = `CREATE TABLE IF NOT EXISTS User (
-    //   id INTEGER PRIMARY KEY NOT NULL UNIQUE,
-    //   email TEXT NOT NULL UNIQUE,
-    //   full_name TEXT NOT NULL,
-    //   coffee_days TEXT DEFAULT 1234,
-    //   skip_next_match INTEGER DEFAULT 0,
-    //   warning_exception INTEGER DEFAULT 0,
-    //   is_active INTEGER DEFAULT 1,
-    //   is_faculty INTEGER DEFAULT 0,
-    //   is_alum INTEGER DEFAULT 0,
-    //   CHECK (is_alum in (0,1)),
-    //   CHECK (is_faculty in (0,1)),
-    //   CHECK (is_active in (0,1)),
-    //   CHECK (skip_next_match in (0,1)),
-    //   CHECK (warning_exception in (0,1))
-    // )`;
-    // const fieldsAsArray: string[] =
-    const fieldsAsArray: string[] = Object.keys(this.fields).map(field => {
-      const {
-        type,
-        isPrimaryKey,
-        isUnique,
-        isNotNull,
-        defaultValue
-      } = this.fields[field];
-      let fieldStr = `${field} ${type}`;
-      if (isPrimaryKey) {
-        fieldStr = fieldStr + ' PRIMARY KEY';
-      }
-      if (isUnique) {
-        fieldStr = fieldStr + ' UNIQUE';
-      }
-      if (isNotNull) {
-        fieldStr = fieldStr + ' NOT NULL';
-      }
-      if (defaultValue && defaultValue !== '') {
-        fieldStr = fieldStr + ' DEFAULT ' + defaultValue;
-      }
+  // public createTable() {
+  //   // const query = `CREATE TABLE IF NOT EXISTS User (
+  //   //   id INTEGER PRIMARY KEY NOT NULL UNIQUE,
+  //   //   email TEXT NOT NULL UNIQUE,
+  //   //   full_name TEXT NOT NULL,
+  //   //   coffee_days TEXT DEFAULT 1234,
+  //   //   skip_next_match INTEGER DEFAULT 0,
+  //   //   warning_exception INTEGER DEFAULT 0,
+  //   //   is_active INTEGER DEFAULT 1,
+  //   //   is_faculty INTEGER DEFAULT 0,
+  //   //   is_alum INTEGER DEFAULT 0,
+  //   //   CHECK (is_alum in (0,1)),
+  //   //   CHECK (is_faculty in (0,1)),
+  //   //   CHECK (is_active in (0,1)),
+  //   //   CHECK (skip_next_match in (0,1)),
+  //   //   CHECK (warning_exception in (0,1))
+  //   // )`;
 
-      return fieldStr;
-    });
+  //   const fieldsAsArray: string[] = Object.keys(this.fields).map(field => {
+  //     const {
+  //       type,
+  //       isPrimaryKey,
+  //       isUnique,
+  //       isNotNull,
+  //       defaultValue
+  //     } = this.fields[field];
+  //     let fieldStr = `${field} ${type}`;
+  //     if (isPrimaryKey) {
+  //       fieldStr = fieldStr + ' PRIMARY KEY';
+  //     }
+  //     if (isUnique) {
+  //       fieldStr = fieldStr + ' UNIQUE';
+  //     }
+  //     if (isNotNull) {
+  //       fieldStr = fieldStr + ' NOT NULL';
+  //     }
+  //     if (defaultValue && defaultValue !== '') {
+  //       fieldStr = fieldStr + ' DEFAULT ' + defaultValue;
+  //     }
 
-    const queryBody = fieldsAsArray.join(',\n');
+  //     return fieldStr;
+  //   });
 
-    const query = `CREATE TABLE IF NOT EXISTS ${this.tableName} (${queryBody})`;
+  //   const queryBody = fieldsAsArray.join(',\n');
 
-    console.log(query);
+  //   const query = `CREATE TABLE IF NOT EXISTS ${this.tableName} (${queryBody})`;
 
-    this.db.exec(query);
-  }
+  //   console.log(query);
+
+  //   this.db.exec(query);
+  // }
 
   // creates a newRecord
   // TODO: make this flexible
@@ -124,5 +121,6 @@ export class UserModel extends Model {
 }
 
 // ========= TESTING!!!! ===============
-const DB_PATH = path.join(__dirname, 'usermodel-test.db');
+const DB_PATH = path.join(__dirname, '-user-model-test.db');
 const user = new UserModel(new sqlite(DB_PATH));
+// user.count();
