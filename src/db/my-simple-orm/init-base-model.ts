49b0c1c44183acd0d64acda1bfe21e33680d1df3
thechutrain <chu.chuyalan@gmail.com>
2019-03-22 19:54:50 -0400

    model factory and  base model intializers

---
+++ src/db/my-simple-orm/init-base-model.ts
@@ -0,0 +1,59 @@
+import sqlite from 'better-sqlite3';
+import * as types from './my-orm-types';
+import * as defaultFn from './default-fn';
+
+/**
+ * Random ideas
+ * - meta property --> store create table schema
+ * - logs property --> all the queries that were executed?
+ */
+
+export function initBaseModel(db: sqlite) {
+  const baseModel = {
+    get primaryKey() {
+      for (const field in this.fields) {
+        if (this.fields[field].isPrimaryKey) {
+          return `${field}`;
+        }
+      }
+      throw new Error('No primary key found');
+    },
+    get foreignKey() {
+      return 'to do....';
+    }
+  };
+
+  Object.defineProperty(baseModel, 'db', {
+    value: db,
+    // NOTE: means that db can't be rewritten for obj downstream of proto-chain
+    writable: false
+  });
+
+  Object.defineProperty(baseModel, 'addQuery', {
+    value: registerCustomQuery,
+    writable: false
+  });
+
+  // Object.defineProperty(baseModel, 'get', {
+  //   writable: false,
+  //   // value(queryArgs: { attrs?: string[]; where?: any }) {
+  //   // this.db.prepare(`SELECT * FROM ${this.tableName}`);
+  //   // }
+  //   value: find.bind(this)
+  // });
+
+  return baseModel;
+}
+
+function registerCustomQuery(queryName, fn: (...argument) => string) {
+  Object.defineProperty(this, queryName, {
+    value() {
+      console.log(this);
+      console.log('this from inside function');
+      const qStr = fn(...arguments);
+      const query = this.db.prepare(qStr);
+      const result = query.exec();
+      return result;
+    }
+  });
+}
