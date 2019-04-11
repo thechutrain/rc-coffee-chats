f0bc6c0b30f6fcbc6495e0a49ac771afca511dbe
thechutrain <chu.chuyalan@gmail.com>
2019-04-11 17:05:51 -0400

    instantiating my user, match, usermatch table in my test

--- src/db/__tests__/user_match.test.ts
+++ src/db/__tests__/user_match.test.ts
@@ -6,6 +6,7 @@ import { UserModel, UserMatchModel, MatchModel } from '../models';
 const DB_NAME = 'user_match_test.db';
 const DB_PATH = path.join(__dirname, 'test_db', DB_NAME);
 let DB_CONNECTION;
+let DB: { User: UserModel; Match: MatchModel; UserMatch: UserMatchModel };
 
 beforeAll(done => {
   let didFail = false;
@@ -21,11 +22,27 @@ beforeAll(done => {
   DB_CONNECTION = new sqlite(DB_PATH);
   expect(DB_CONNECTION.open).toBe(true);
 
+  // Instantiate the User, UserMatch, Match models:
+  DB = {
+    User: new UserModel(DB_CONNECTION),
+    Match: new MatchModel(DB_CONNECTION),
+    UserMatch: new UserMatchModel(DB_CONNECTION)
+  };
+
   done();
 });
 
+/**
+ * Things my test should cover:
+ * [] addMatch
+ * [] find previous matches for a given user
+ * [] find all the people who want to be matched for today
+ *
+ */
 describe('Integration Test of User, UserMatch, Match Table:', () => {
   it('should just work!', () => {
     expect(true).toBe(true);
   });
+
+  it('should be able to find users to match for today', () => {});
 });
