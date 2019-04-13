155dc310038fd81cb524a0a149039f0c0ff3458b
thechutrain <chu.chuyalan@gmail.com>
2019-04-13 16:58:01 -0400

    migration script, gets current users

--- data/migrations/04_13/migrate_from_glitch_v2.ts
+++ data/migrations/04_13/migrate_from_glitch_v2.ts
@@ -13,19 +13,83 @@ const migratedDB = initDB(pathToMigrated, false);
 // Update the users table
 ///////////////////////////////////
 // 1) Get all users from glitch database and add them to migrated DB
+
 const glitchUsers = glitchDB.prepare('Select * from Users').all(); // 71 users
 glitchUsers.forEach(user => {
-  // console.log(user.email);
-  // console.log(user.coffee_days);
   migratedDB.User.add({
     email: user.email,
     full_name: user.email,
-    coffee_days: user.coffee_days,
+    coffee_days: `${user.coffee_days}`,
     is_active: '0'
   });
 });
 
 // 2) find all users in the activeUser list and see if they are in the users database
 // update their active status to be 1
+let foundActiveUsers = 0;
+const usersNotFound: any[] = [];
+activeUsers.forEach(email => {
+  try {
+    migratedDB.User.findByEmail(email);
+    migratedDB.User.update({ is_active: '1' }, { email });
+    foundActiveUsers += 1;
+  } catch (e) {
+    usersNotFound.push(email);
+  }
+});
+// console.log(activeUsers); // 26
+// console.log(usersNotFound.length); // 23
 
-// migratedDB.User.add({});
+// For users active on the stream but not in the database
+usersNotFound.forEach(email => {
+  const { changes } = migratedDB.User.add({
+    email,
+    full_name: email,
+    coffee_days: '1234',
+    is_active: '1'
+  });
+  if (changes !== 1) {
+    console.log(`Could not add in user: ${email}`);
+  }
+});
+
+const total_users = migratedDB.User.count();
+console.log(`Total number of users: ${total_users}`);
+
+////////////////////////////////////
+// Update all the previous matches
+///////////////////////////////////
+const all_matches = glitchDB.prepare('SELECT * from Matches').all();
+// console.log(all_matches.length); // 1765
+
+// console.log(all_matches[0]); // date, email1, email2;
+let skip_matches = 0;
+let matches = 0;
+all_matches.forEach(match_record => {
+  const { date, email1, email2 } = match_record;
+  let user_1;
+  let user_2;
+  let skip = false;
+  try {
+    user_1 = migratedDB.User.findByEmail(email1);
+    user_2 = migratedDB.User.findByEmail(email2);
+  } catch (e) {
+    skip_matches += 1;
+    skip = true;
+  }
+  if (!skip) {
+    try {
+      migratedDB.UserMatch.addNewMatch([user_1.id, user_2.id], date);
+      matches += 1;
+    } catch (e) {
+      console.log('========== ERROR ===========');
+      console.log(e);
+      console.log(user_1.email);
+      console.log(user_2.email);
+    }
+  }
+});
+
+console.log('=========');
+console.log(skip_matches); // 1246
+console.log(matches); // 519
