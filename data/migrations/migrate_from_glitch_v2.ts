3bf4d822caef8f28ff7f548704bf70cb77475c91
thechutrain <chu.chuyalan@gmail.com>
2019-04-13 15:40:51 -0400

    beginning the migration of current data

---
+++ data/migrations/migrate_from_glitch_v2.ts
@@ -0,0 +1,6 @@
+import sqlite from 'better-sqlite3';
+import * as path from 'path';
+
+import activeUsers from './activeUsers04_13';
+
+console.log(activeUsers);
---
+++ src/chrons/make-matches.ts
@@ -0,0 +1,4 @@
+/**
+ * make matches chron job
+ *
+ */
