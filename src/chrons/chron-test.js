df937751a756eb73be738aafa56cb1009e38e6cb
thechutrain <chu.chuyalan@gmail.com>
2019-04-16 14:20:31 -0400

    made a chron to test

---
+++ src/chrons/chron-test.js
@@ -0,0 +1,9 @@
+const now = new Date();
+const timeAsString = now.toLocaleTimeString('en-US', {
+  timeZone: 'America/New_York'
+});
+const dateAsString = now.toLocaleDateString('en-US', {
+  timeZone: 'America/New_York'
+});
+const chronLog = `Chron ran: ${dateAsString} @ ${timeAsString}`;
+console.log(chronLog);
