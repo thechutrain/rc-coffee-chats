74c98cc408461d71eb3cb03c25b841c151b9fc5c
thechutrain <chu.chuyalan@gmail.com>
2019-04-16 00:41:04 -0400

    fixed the typing sign on findUserPriority, single generic. Can't use indexOf since im passing in an object ...

--- src/chrons/make-matches/__tests__/find-user-priority.test.ts
+++ src/chrons/make-matches/__tests__/find-user-priority.test.ts
@@ -12,18 +12,23 @@ describe('findUserPriority(): ', () => {
       email: 'a',
       prevMatches: []
     };
-    const oppositePool = ['w_1', 'w_2', 'w_3', 'w_4'];
+    const oppositePool = [
+      { email: 'w_1', prevMatches: [] },
+      { email: 'w_2', prevMatches: [] },
+      { email: 'w_3', prevMatches: [] },
+      { email: 'w_4', prevMatches: [] }
+    ];
 
-    const user_a_priority = findUserPriority<basicUser, string>(
-      user_a,
-      oppositePool
-    );
+    const user_a_priority = findUserPriority<basicUser>(user_a, oppositePool);
+
     // TEST: make sure the returned priority list is the same length of given oppositePool
     expect(user_a_priority.length).toBe(oppositePool.length);
+
     // TEST: ensure the the priority list includes each person in the opposite pool
-    oppositePool.forEach(email => {
-      const index = user_a_priority.indexOf(email);
-      expect(index).not.toBe(-1);
+    oppositePool.forEach(oppositeUser => {
+      expect(user_a_priority).toEqual(
+        expect.arrayContaining([expect.objectContaining(oppositeUser)])
+      );
     });
   });
 
@@ -40,26 +45,68 @@ describe('findUserPriority(): ', () => {
         {
           email: 'w_3'
         }
+        // {
+        //   email: 'I dont exist in the opposite pool of users'
+        // }
       ]
     };
-    const oppositePool = ['w_1', 'w_2', 'w_3'];
+    const oppositePool = [
+      { email: 'w_1', prevMatches: [] },
+      { email: 'w_2', prevMatches: [] },
+      { email: 'w_3', prevMatches: [] }
+    ];
 
-    const user_a_priority = findUserPriority<basicUser, string>(
-      user_a,
-      oppositePool
-    );
+    const user_a_priority = findUserPriority<basicUser>(user_a, oppositePool);
 
     for (let i = 0; i < user_a_priority.length; i++) {
-      expect(user_a_priority[i]).toBe(oppositePool[i]);
+      expect(user_a_priority[i]).toEqual(oppositePool[i]);
     }
 
     // TEST: make sure the returned priority list is the same length of given oppositePool
     expect(user_a_priority.length).toBe(oppositePool.length);
+
     // TEST: ensure the the priority list includes each person in the opposite pool
-    oppositePool.forEach(email => {
-      const index = user_a_priority.indexOf(email);
-      expect(index).not.toBe(-1);
-    });
+  });
+
+  it('should return prevMatches, if user has matched with everyone in the opposite pool and other non relative people as well', () => {
+    const user_a = {
+      email: 'a',
+      prevMatches: [
+        {
+          email: 'w_1'
+        },
+        {
+          email: 'w_2'
+        },
+        {
+          email: 'w_3'
+        },
+        {
+          email: 'I dont exist in the opposite pool of users'
+        },
+        {
+          email: 'Another nonrelevant person'
+        }
+      ]
+    };
+    const oppositePool = [
+      { email: 'w_1', prevMatches: [] },
+      { email: 'w_2', prevMatches: [] },
+      { email: 'w_3', prevMatches: [] }
+    ];
+
+    const user_a_priority = findUserPriority<basicUser>(user_a, oppositePool);
+
+    for (let i = 0; i < user_a_priority.length; i++) {
+      expect(user_a_priority[i]).toEqual(oppositePool[i]);
+    }
+
+    // console.log(user_a_priority);
+
+    // TEST: make sure the returned priority list is the same length of given oppositePool
+    expect(user_a_priority.length).toBe(oppositePool.length);
+
+    // TEST: ensure the the priority list includes each person in the opposite pool
   });
 
   it('should priorities users that one has not met with yet', () => {
@@ -71,11 +118,15 @@ describe('findUserPriority(): ', () => {
         }
       ]
     };
-    const oppositePool = ['w_1', 'unique match 1', 'unique match 2'];
+    const oppositePool = [
+      { email: 'w_1', prevMatches: [] },
+      { email: 'unique match 1', prevMatches: [] },
+      { email: 'unique match 2', prevMatches: [] }
+    ];
 
-    const priority = findUserPriority<basicUser, string>(user_a, oppositePool);
+    const priority = findUserPriority<basicUser>(user_a, oppositePool);
 
     const last_index = priority.length - 1;
-    expect(priority[last_index]).toBe('w_1');
+    expect(priority[last_index].email).toBe('w_1');
   });
 });
--- src/chrons/make-matches/create-suitor-acceptor-pool.ts
+++ src/chrons/make-matches/create-suitor-acceptor-pool.ts
@@ -10,7 +10,6 @@ import {
   makeSuitorPool,
   makeAcceptorPool
 } from '../../matching-algo/stable-marriage-matching/helper-fn-marriage';
-import { deepClone } from '../../utils/clone';
 
 export type basicPrevMatch = {
   email: string;
@@ -22,34 +21,39 @@ export type basicUser = {
 };
 
 // ✅ tested!
-export function findUserPriority<U extends basicUser, E>(
+export function findUserPriority<U extends basicUser>(
   currUser: U,
-  oppositePool: E[]
-): E[] {
-  const requiredEmails = deepClone(oppositePool);
-  const uniqueMatches = [];
-  const prevMatches = [];
+  _requiredUsersForPriority: U[]
+): U[] {
+  const requiredUsers: U[] = cloneDeep(_requiredUsersForPriority);
+  const uniqueMatches: U[] = [];
+  const nonUniqueMatches: U[] = [];
 
   // Note: SQL query returns prevMatches based on last date, preserve this order
-  // when making a user's priority
   currUser.prevMatches.forEach((prevMatch: basicPrevMatch) => {
-    const { email } = prevMatch;
-    // Check if this email is in the requireEmails
-    const indexOfMatch = requiredEmails.indexOf(email);
+    // Note: current user's previous match may or may not be in the pool of requiredUsersForPriority since they are split randomly into two separate sets
+    const prevMatchEmail = prevMatch.email;
+    let prevMatchFound = -1;
+    for (let i = 0; i < requiredUsers.length; i++) {
+      if (prevMatchEmail === requiredUsers[i].email) {
+        prevMatchFound = i;
+        break;
+      }
+    }
 
-    if (indexOfMatch >= 0) {
-      prevMatches.push(email);
-      requiredEmails.splice(indexOfMatch, 1); // also remove them from requiredEmails
+    if (prevMatchFound !== -1) {
+      // Note: if a user from the requiredUserPool is found as a previous match of currUser, then remove them from the requiredUser list & put them into the prevMatch
+      nonUniqueMatches.push(requiredUsers.splice(prevMatchFound, 1)[0]);
     }
   });
 
-  return [...shuffle(requiredEmails), ...prevMatches];
+  return [...shuffle(requiredUsers), ...nonUniqueMatches];
 }
 
-// ✅ tested!
+// ✅ tested!1
 export function createSuitorAcceptorPool<A extends basicUser>(
   users: A[],
-  fallBackPerson: A
+  fallBackPerson: basicUser
 ): {
   suitors: Map<mTypes.marriage_id, mTypes.Suitor<A>>;
   acceptors: Map<mTypes.marriage_id, mTypes.Acceptor<A>>;
@@ -59,7 +63,8 @@ export function createSuitorAcceptorPool<A extends basicUser>(
   let fallBackMatch: null | A = null;
   if (!isEvenNumberUsers(userCopy)) {
     // TODO: Improved feature that removes a user who hasn't matched with fallback person!
-    fallBackMatch = userCopy.splice(users.length - 1, 1)[0];
+    const fallBackIndex = findFallBackMatch(userCopy, fallBackPerson);
+    fallBackMatch = userCopy.splice(fallBackIndex, 1)[0];
   }
 
   const shuffledUsers = shuffle(userCopy);
@@ -87,16 +92,35 @@ export function createSuitorAcceptorPool<A extends basicUser>(
   };
 }
 
-/** TODO: findFallBackMatch
+/**
+ * ✳️ EDGE CASE: if the fallbackPerson isn't someone who was suppose to be
+ *  matched for today, they will not show up in another one's previous matches
+ *
+ *  TODO: Need to fix the sql query for getting prevMatches!
+ *
  *
  */
+export function findFallBackMatch<A extends basicUser>(
+  people: A[],
+  fallBackPerson: A
+): number {
+  // let match: A | null = null;
+  for (let i = people.length - 1; i >= 0; i--) {
+    let haveMatched = false;
+    for (const prevMatch of people[i].prevMatches) {
+      if (prevMatch.email === fallBackPerson.email) {
+        haveMatched = true;
+        break;
+      }
+    }
 
-// export function findFallBackMatch<A extends basicUser>(
-//   people: A[],
-//   fallBackPerson: A
-// ) {
-//   let i = people.length - 1;
-// }
+    if (!haveMatched) {
+      return i;
+    }
+  }
+
+  return 0;
+}
 
 export function isEvenNumberUsers<A>(people: A[]): boolean {
   return people.length % 2 === 0;
