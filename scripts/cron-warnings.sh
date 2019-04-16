e157c3d9d52a18c5a668343e2c9e3096f73d6afd
thechutrain <chu.chuyalan@gmail.com>
2019-04-16 16:40:44 -0400

    fixed the date formatting issues

--- scripts/cron-warnings.sh
+++ scripts/cron-warnings.sh
@@ -3,8 +3,9 @@
 # runs chron job for sending out warnings that people will be matched the next day
 
 cd ~/rc-coffee-chats
-current_date_time="`date +%Y-%m-%d %H:%M:%S`";
+current_date_time=`date "+%Y-%m-%d %H:%M:%S"`;
 echo "\n=================\nTime is: $current_date_time \nStarting warnings cron job ..." > /root/cron-jobs/logs
 npm run cron-warnings> /root/cron-jobs/logs
-current_date_time="`date +%Y-%m-%d %H:%M:%S`";
+
+current_date_time=`date "+%Y-%m-%d %H:%M:%S"`;
 echo "\nFinished warnings cron job \n$current_date_time --> ran matchify chron :)\n============" > root/cron-jobs/logs;
