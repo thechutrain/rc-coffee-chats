088de6ad25c43cfa1a4cc40519a1d3d1f2c51ca8
thechutrain <chu.chuyalan@gmail.com>
2019-04-17 19:23:27 -0400

    not actually calling the function!

--- src/crons/notify-users/index.ts
+++ src/crons/notify-users/index.ts
@@ -1,21 +1,27 @@
 /** NOTE: not a cron job, but a script that will notify users of message:
  *
  */
+import * as dotenv from 'dotenv-safe';
+dotenv.config();
+
 import { initDB } from '../../db';
 import { sendGenericMessage } from '../../zulip-messenger/msg-sender';
 
-export function sendUserNotification(message: string) {
+function sendUserNotification(msg: string) {
   const db = initDB();
   const activeUsers = db.User.findActiveUsers();
 
   console.log(activeUsers);
 
   activeUsers.forEach(user => {
-    const customMessage = `Hello and welcome to Coffee Chat 2.0 ☕️. I've been brewing for a while, but just like our coffee pot there may be some cracks in me. If you find a 🐞 don't worry, just post an issue on github [here](https://github.com/thechutrain/rc-coffee-chats/issues).
-    \nYou can also find all my code and documentation on github: [https://github.com/thechutrain/rc-coffee-chats](https://github.com/thechutrain/rc-coffee-chats)
-    \nBut if you don't have time to read about my new changes, you can ask for **HELP**.
-    No coffee pots were harmed in the making of this app.
-    `;
-    sendGenericMessage(user.email, customMessage);
+    sendGenericMessage(user.email, msg);
   });
 }
+
+const customMessage = `Hello and welcome to Coffee Chat 2.0 ☕️. I've been brewing for a while, but just like our coffee pot there may be some cracks in me. If you find a 🐞 don't worry, just post an issue on github [here](https://github.com/thechutrain/rc-coffee-chats/issues).
+\nYou can also find all my code and documentation on github: [https://github.com/thechutrain/rc-coffee-chats](https://github.com/thechutrain/rc-coffee-chats)
+\nBut if you don't have time to read about my new changes, you can ask for **HELP**.
+No coffee pots were harmed in the making of this app.
+`;
+
+sendUserNotification(customMessage);
