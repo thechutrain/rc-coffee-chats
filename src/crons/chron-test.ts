b429068b4ecf61b33caf228f135f3363e941d096
thechutrain <chu.chuyalan@gmail.com>
2019-04-17 19:13:34 -0400

    finished the notify users

--- src/crons/chron-test.ts
+++
@@ -1,9 +0,0 @@
-const now = new Date();
-const timeAsString = now.toLocaleTimeString('en-US', {
-  timeZone: 'America/New_York'
-});
-const dateAsString = now.toLocaleDateString('en-US', {
-  timeZone: 'America/New_York'
-});
-const chronLog = `Chron ran: ${dateAsString} @ ${timeAsString}`;
-console.log('=================================\n' + chronLog + '\n');
