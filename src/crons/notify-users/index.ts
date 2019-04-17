f08b0f335c52749a955307f5b73fd9f4ee30ac54
thechutrain <chu.chuyalan@gmail.com>
2019-04-17 19:28:32 -0400

    changed message content for notifying all users

--- src/crons/notify-users/index.ts
+++ src/crons/notify-users/index.ts
@@ -18,10 +18,9 @@ function sendUserNotification(msg: string) {
   });
 }
 
-const customMessage = `Hello and welcome to Coffee Chat 2.0 ☕️. I've been brewing for a while, but just like our coffee pot there may be some cracks in me. If you find a 🐞 don't worry, just post an issue on github [here](https://github.com/thechutrain/rc-coffee-chats/issues).
+const customMessage = `Hello and welcome to Coffee Chat 2.0 ☕️. \nI've been brewing for a while, but just like our coffee pot there may be some cracks in me. If you find a 🐞 don't worry, just post an issue on github [here](https://github.com/thechutrain/rc-coffee-chats/issues).
 \nYou can also find all my code and documentation on github: [https://github.com/thechutrain/rc-coffee-chats](https://github.com/thechutrain/rc-coffee-chats)
-\nBut if you don't have time to read about my new changes, you can ask for **HELP**.
-No coffee pots were harmed in the making of this app.
+\nYou can also type **HELP** to learn more.
 `;
 
 sendUserNotification(customMessage);
