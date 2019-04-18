ceacdd138c97bbd003cf3abfe2f4a1c8a6bf1bbd
thechutrain <chu.chuyalan@gmail.com>
2019-04-18 11:46:28 -0400

    finished warning message cron

--- src/crons/match-warnings/index.ts
+++ src/crons/match-warnings/index.ts
@@ -5,14 +5,31 @@ dotenv.config();
 
 import { initDB } from '../../db';
 import { UserRecord } from '../../db/dbTypes';
+import { sendGenericMessage } from '../../zulip-messenger/msg-sender';
 
-const usersToWarn: UserRecord[] = (() => {
-  const db = initDB();
-  const today = moment()
-    .tz('America/New_York')
-    .day();
+function sendNextDayMatchWarning(sendMessage = true) {
+  const usersToWarn: UserRecord[] = (() => {
+    const db = initDB();
+    const today = moment()
+      .tz('America/New_York')
+      .day();
 
-  return db.User.findUsersNextDayMatchWarning(today);
-})();
+    return db.User.findUsersNextDayMatchWarning(today);
+  })();
 
-console.log(usersToWarn);
+  console.log('Users to warn:');
+  console.log(usersToWarn);
+
+  const warningMessage = `Just a friendly reminder that you'll be matched tomorrow for coffee chats
+  If you can't meet tomorrow and would like to cancel tomorrow's match, just type: \`\`\`**UPDATE SKIP 0**\`\`\`
+  If you would no longer wish to receive these warnings messages, just type: \`\`\`**UPDATE WARNINGS 0**\`\`\`
+  `;
+
+  if (sendMessage) {
+    usersToWarn.forEach(user => {
+      sendGenericMessage(user.email, warningMessage);
+    });
+  }
+}
+
+sendNextDayMatchWarning();
