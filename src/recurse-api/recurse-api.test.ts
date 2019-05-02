ec6232eb772c36c5ca9462203cb12dc8b9e77eb8
thechutrain <chu.chuyalan@gmail.com>
2019-05-02 19:07:13 -0400

    fixed up test for what I want

--- src/recurse-api/recurse-api.test.ts
+++ src/recurse-api/recurse-api.test.ts
@@ -1,7 +1,7 @@
 import { getNewCurrentBatches } from './index';
 
 describe('rc utility function', () => {
-  it('should be able to get start dates', async () => {
+  it('should be able to get batches that started today', async () => {
     // expect(false).toBe(true);
     const { newBatches, currentBatches } = await getNewCurrentBatches(
       '2019-04-01'
@@ -10,12 +10,30 @@ describe('rc utility function', () => {
     expect(newBatches.length).toBe(2);
   });
 
-  it('should be able to get ending dates', async () => {
+  it('should not get any batches did not start today', async () => {
     // expect(false).toBe(true);
     const { newBatches, currentBatches } = await getNewCurrentBatches(
-      '2019-06-27'
+      '2019-04-02'
+    );
+
+    expect(newBatches.length).toBe(0);
+  });
+
+  it('should be able to get batches that are insession', async () => {
+    // expect(false).toBe(true);
+    const { newBatches, currentBatches } = await getNewCurrentBatches(
+      '2019-04-02'
     );
     expect(newBatches.length).toBe(0);
-    expect(currentBatches.length).toBe(1);
+    expect(currentBatches.length).toBe(3);
+  });
+
+  it('should include batches that are ending on given day', async () => {
+    const { newBatches, currentBatches } = await getNewCurrentBatches(
+      '2019-03-28'
+    );
+
+    expect(newBatches.length).toBe(0);
+    expect(currentBatches.length).toBe(2);
   });
 });
