0a5e4bcfa810c9e79cd6395dcfa893e1632f91b3
thechutrain <chu.chuyalan@gmail.com>
2019-05-02 01:15:56 -0400

    nice little log message

--- src/crons/matchify/notify_admin.ts
+++ src/crons/matchify/notify_admin.ts
@@ -5,6 +5,6 @@ import { sendGenericMessage } from '../../zulip-messenger/msg-sender';
 export function notifyAdmin(message: string) {
   const db = initDB();
   const adminEmailList = db.User.findAdmins().map(adminUser => {
-    sendGenericMessage(adminUser.email, message);
+    sendGenericMessage(adminUser.email, '🌲 LOG MESSAGE 🌲\n' + message);
   });
 }
