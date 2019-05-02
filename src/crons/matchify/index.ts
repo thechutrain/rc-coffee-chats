3b097a991919e3967e8db47d610a315eeda9c577
thechutrain <chu.chuyalan@gmail.com>
2019-05-02 15:32:01 -0400

    scaffolded special case days foro the matchifying

--- src/crons/matchify/index.ts
+++ src/crons/matchify/index.ts
@@ -28,18 +28,32 @@ const startTime = moment()
 const logArray = [`====== makeMatches() @ ${startTime} =====`];
 const REPEAT_MATCHES: Array<[string, string]> = [];
 
-makeMatches(false);
-async function makeMatches(runForReal = true) {
+matchify(false);
+async function matchify(runForReal = true) {
   const db = initDB();
 
-  // TODO: Check if today is an exception or not!
+  /** TODO: Exception & Special Day handling
+   * Special day cases:
+   * a) First Day of Batch  --> do not match && notify users who planned on matching today?
+   * b) Last Day of Batch   --> deactivate all users automatically & notify them offboarding.
+   * c) Holidays --> warn the day before? Do Later ...
+   */
   const { starting_batches, ending_batches } = await getStartingEndingBatches();
   if (!starting_batches.length) {
     // It's the first day! default do not run chat bot!
     console.log('Its the first day of the batch!!!');
+    /** TODO: tasks for the first day
+     * - notify all users who were suppose to be matched today that they won't have a match
+     * - warn the maintainers that today is the first day so matches are skipped... spread the word!
+     */
   } else if (!ending_batches.length) {
     // Its the last day! Off-board active users.
-    // TODO: offboarding messaging.
+    /** TODO: tasks for end of batch
+     * - find all users of chat bot who are also in this last batch
+     * - notify them that they have been deactivated, but if they want to stay in touch. welcome to!
+     * - warn matinainer that today is the last day of the batch? And everything is Okay.
+     *
+     */
     console.log('Its the end of the batch!!');
   }
 
