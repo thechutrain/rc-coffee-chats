9a8a074d9146b7ca337700bbc79267fa280ec2d9
thechutrain <chu.chuyalan@gmail.com>
2019-05-02 01:10:40 -0400

    removed trees in log message

--- src/crons/matchify/notify_admin.ts
+++ src/crons/matchify/notify_admin.ts
@@ -5,6 +5,6 @@ import { sendGenericMessage } from '../../zulip-messenger/msg-sender';
 export function notifyAdmin(message: string) {
   const db = initDB();
   const adminEmailList = db.User.findAdmins().map(adminUser => {
-    sendGenericMessage(adminUser.email, 'LOG MESSAGE! 🌲🌲🌲' + message);
+    sendGenericMessage(adminUser.email, message);
   });
 }
