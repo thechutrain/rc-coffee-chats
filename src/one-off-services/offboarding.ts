475ecd7be8cbb43196b660654a192440afd6cb25
thechutrain <chu.chuyalan@gmail.com>
2019-05-02 15:32:14 -0400

    scaffold todo for offBoardUsers

---
+++ src/one-off-services/offboarding.ts
@@ -0,0 +1,14 @@
+/** function used for deactivating users, notifying them, updating maintainers that
+ *
+ * @param batchId
+ */
+
+export function offBoardUsers(batchId: number) {
+  /**
+   * - find all users in the given batch
+   * - find all active users who are in the current batch
+   * - deactivate all of these users
+   * - notify all of the users they have been deactivated
+   * - send this logging information to admins
+   */
+}
