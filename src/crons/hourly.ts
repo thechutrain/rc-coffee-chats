1be78fb04825fb3b218a5867bea98fb2ef49a30d
thechutrain <chu.chuyalan@gmail.com>
2019-05-04 16:23:51 -0400

    another test

--- src/crons/hourly.ts
+++ src/crons/hourly.ts
@@ -11,6 +11,8 @@ function hourly() {
   // Run at 8pm EST
   if (hour === 8) {
     // run matching
+  } else if (hour === 16) {
+    console.log('testing ... the hour of 4pm');
   } else if (hour === 17) {
     console.log('running at 5pm yooo');
     handlePossibleOffboarding();
