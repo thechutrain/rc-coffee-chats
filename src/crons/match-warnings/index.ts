16f40deaa2c05457b9509cecee5e89665897b7fa
thechutrain <chu.chuyalan@gmail.com>
2019-05-04 17:37:25 -0400

    templateMessageSender checks if in prod or not, so no need to check NODE_ENV

--- src/crons/match-warnings/index.ts
+++ src/crons/match-warnings/index.ts
@@ -8,7 +8,6 @@ import { UserRecord } from '../../db/dbTypes';
 import { templateMessageSender } from '../../zulip-messenger/msg-sender';
 
 export function sendNextDayMatchWarning() {
-  const isProd = process.env.NODE_ENV === 'production';
   const usersToWarn: UserRecord[] = (() => {
     const db = initDB();
     const today = moment()
@@ -21,9 +20,7 @@ export function sendNextDayMatchWarning() {
   console.log('Users to warn:');
   console.log(usersToWarn);
 
-  if (isProd) {
-    usersToWarn.forEach(user => {
-      templateMessageSender(user.email, types.msgTemplate.WARNING_NOTIFICATION);
-    });
-  }
+  usersToWarn.forEach(user => {
+    templateMessageSender(user.email, types.msgTemplate.WARNING_NOTIFICATION);
+  });
 }
