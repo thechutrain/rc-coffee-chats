91e83e6f6895556787f2c0e33c0abd618b5b715a
thechutrain <chu.chuyalan@gmail.com>
2019-05-04 23:01:44 -0400

    updated dates file

--- src/utils/dates.ts
+++ src/utils/dates.ts
@@ -1,4 +1,7 @@
-import moment from 'moment';
+import moment from 'moment-timezone';
 
 export const isToday = (date: string): boolean =>
   moment().diff(moment(date), 'days') === 0;
+
+// TESTING
+// console.log(isToday('2019-05-04'));
