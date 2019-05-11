744be44ac52fb6e4e61c725a7b21e3d80133df4f
thechutrain <chu.chuyalan@gmail.com>
2019-05-11 11:18:54 -0400

    cleaned up console logs

--- src/middleware/zulip-token-validator.ts
+++ src/middleware/zulip-token-validator.ts
@@ -1,15 +1,10 @@
 import * as types from '../types';
 
 export function zulipTokenValidator(req: types.IZulipRequest, res, next) {
-  console.log('token!!');
-  console.log(req.body.token);
   if (req.body.token !== process.env.ZULIP_BOT_TOKEN) {
-    console.log('token is not valid');
     req.local.errors.push({
       errorType: types.Errors.INVALID_ZULIP_TOKEN
     });
-  } else {
-    console.log('TOKEN IS VALID');
   }
 
   next();
