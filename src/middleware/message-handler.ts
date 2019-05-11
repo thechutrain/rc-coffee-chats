bb676c0d94d58f5890a87ffbde8f640c793fa91e
thechutrain <chu.chuyalan@gmail.com>
2019-05-10 21:55:29 -0400

    awaiting on axios promise

--- src/middleware/message-handler.ts
+++ src/middleware/message-handler.ts
@@ -8,24 +8,26 @@ import {
 } from '../zulip-messenger/msg-sender';
 import * as types from '../types';
 
-export function messageHandler(req: types.IZulipRequest, res, next) {
+export async function messageHandler(req: types.IZulipRequest, res, next) {
   const originUser = req.local.user.email;
   // const { targetUser } = req.local.action;
   const { errors } = req.local;
+  console.log(errors);
 
   // Case: handle error messages
   // NOTE: originUser may not be there if bad token
   if (originUser && errors.length) {
-    errors.forEach(err => {
+    errors.forEach(async err => {
       const messageContent = err.customMessage
         ? `${err.customMessage}`
         : `Error: unspecified error ... ooops!`;
       try {
-        sendGenericMessage(originUser, messageContent);
+        await sendGenericMessage(originUser, messageContent);
       } catch (e) {
-        console.warn(`Error trying to sendGenericMEssage: ${e}`);
+        console.warn(`Error trying to sendGenericMessage: ${e}`);
       }
     });
+
     next();
     return;
   }
@@ -33,11 +35,15 @@ export function messageHandler(req: types.IZulipRequest, res, next) {
   // Case: given a msgType
   const { msgTemplate, sendTo, msgArgs } = req.local.msgInfo;
 
-  if (msgTemplate in types.msgTemplate) {
-    templateMessageSender(sendTo, msgTemplate, msgArgs);
-  } else {
-    const msg = JSON.stringify(req.local.msgInfo);
-    sendGenericMessage(originUser, `Req.local.msgInfo: ${msg}`);
+  try {
+    if (msgTemplate in types.msgTemplate) {
+      await templateMessageSender(sendTo, msgTemplate, msgArgs);
+    } else {
+      const msg = JSON.stringify(req.local.msgInfo);
+      await sendGenericMessage(originUser, `Req.local.msgInfo: ${msg}`);
+    }
+  } catch (e) {
+    console.warn(`Error trying to send a message!!!!`, req.local.msgInfo);
   }
   next();
 }
