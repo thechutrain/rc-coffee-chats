2ec31426d6887e6ace4b82ba10e98965ffdcca4e
thechutrain <chu.chuyalan@gmail.com>
2019-05-12 09:49:57 -0400

    timing out issues with db test, possibly bc you cant have nested transactions in sqlite?

--- src/db/__tests__/user.test.ts
+++ src/db/__tests__/user.test.ts
@@ -18,17 +18,20 @@ describe('Updated User tests:', () => {
     User = DB.User;
   });
 
-  beforeEach(done => {
-    const begin = DB.DB_CONNECTION.prepare('BEGIN');
-    begin.run();
-    done();
-  });
+  // NOTE: can't begin multiple transactions in better sqlite!
+  // This hack of not committing any records in the DB won't be able
+  // to work with any update functions
+  // beforeEach(done => {
+  //   const begin = DB.DB_CONNECTION.prepare('BEGIN');
+  //   // begin.run();
+  //   done();
+  // });
 
-  afterEach(done => {
-    const rollback = DB.DB_CONNECTION.prepare('ROLLBACK');
-    rollback.run();
-    done();
-  });
+  // afterEach(done => {
+  //   const rollback = DB.DB_CONNECTION.prepare('ROLLBACK');
+  //   // rollback.run();
+  //   done();
+  // });
 
   afterAll(done => {
     DB.DB_CONNECTION.close();
@@ -37,10 +40,33 @@ describe('Updated User tests:', () => {
   });
 
   /** Beginning of the actual Tests
-   *
+   * Issues with timing out!
    */
   xit('should be able to find all the users who are planning on skipping', () => {
+    const seedUsers = [
+      {
+        email: 'al@recurse.com',
+        skipNext: true,
+        days: [1, 2]
+      },
+      {
+        email: 'b@recurse.com',
+        days: [1]
+      }
+    ];
+    for (const user of seedUsers) {
+      User.add({
+        email: user.email,
+        full_name: 'anon'
+      });
+      // if (user.skipNext) {
+      //   User.updateSkipNextMatch(user.email, true);
+      // }
+      User.updateDays(user.email, user.days);
+    }
+
+    // Tests:
     const skippers = User.usersToSkip(1);
-    expect(skippers).toBe(false);
+    expect(skippers.length).toBe(1);
   });
 });
