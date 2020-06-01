535cbc213995aa20deb7e347c55fb39ef88f3436
James J Porter <james@jamesporter.me>
2020-06-01 15:08:27 -0400

    Revert "Revert "send matches the previous day (#103)" (#104)"
    This reverts commit a19e388ffa08cc583ccc8d3f2609eaf206d9d093.

--- src/crons/matchify/notify-match-pair.ts
+++ src/crons/matchify/notify-match-pair.ts
@@ -27,9 +27,9 @@ export function notifyMatchPair(match: matchPair) {
 
   // TODO: add links to the names of the users here
   const msgContent =
-    `☀️ Good morning ${nameLinks.join(
+    `Hello ${nameLinks.join(
       ' and '
-    )}! \nThe two of you have been paired for a chat today. Hope you get a chance to chat over coffee or tea or anything that you fancy -- enjoy!` +
+    )}! \nYou have been paired for a chat!` +
     '\n\n' +
     zoomMessage;
 
